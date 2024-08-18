74d99b8dc2fe52183420b947117ff2dae5ad2e28
jebin2 <jebineinstein@gmail.com>
2024-08-18 23:03:39 +0530

    Updates

---
+++ precache-manifest.2a07c07fb4fabc418af20336235ce48b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3373bcecf0307218cc73cc6f928ec6ec",
+    "url": "/cardholder/index.html"
+  },
+  {
+    "revision": "02b3bdda1d26879258ff",
+    "url": "/cardholder/static/css/main.354bd72a.chunk.css"
+  },
+  {
+    "revision": "c9a030f8c959d9062054",
+    "url": "/cardholder/static/js/2.aa539e85.chunk.js"
+  },
+  {
+    "revision": "85b57216247c06f1d678660f55e0f4fd",
+    "url": "/cardholder/static/js/2.aa539e85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02b3bdda1d26879258ff",
+    "url": "/cardholder/static/js/main.05a7fba8.chunk.js"
+  },
+  {
+    "revision": "8e26e99c280b13a49c71",
+    "url": "/cardholder/static/js/runtime-main.5be9f528.js"
+  }
+]);

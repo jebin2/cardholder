2f2d243d9071d7f5a96b516ef650011cc5040dc7
jebin2 <jebineinstein@gmail.com>
2024-08-24 13:40:00 +0530

    test service worker.

--- workbox-config.js
+++ workbox-config.js
@@ -5,18 +5,6 @@ module.exports = {
 	],
 	swDest: 'build/service-worker.js',
 	swSrc: 'src/service-worker.js',
-	runtimeCaching: [
-	  {
-		urlPattern: ({ url }) => url.origin === self.location.origin && url.pathname.endsWith('.png'),
-		handler: 'StaleWhileRevalidate',
-		options: {
-		  cacheName: 'images',
-		  expiration: {
-			maxEntries: 50,
-		  },
-		},
-	  },
-	],
 	// Optional: Other configuration options for Workbox
   };
   

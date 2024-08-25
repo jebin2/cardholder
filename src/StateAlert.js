bbe4a640d8ed84503f2c7e6a46db35c141bb258a
jebin2 <jebineinstein@gmail.com>
2024-08-25 19:52:16 +0530

    color change.

--- src/StateAlert.js
+++ src/StateAlert.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import {
-    Snackbar, Alert
+	Snackbar, Alert
 } from '@mui/material';
 
 export default function StateAlert({ backgroundColor, state, type, message, setAlertState }) {
-    return (
-        <Snackbar open={state} autoHideDuration={5000} onClose={() => setAlertState(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-            sx={{
-                '& .MuiPaper-root': {
-                  background: `${backgroundColor}`,
-                  border: "6px solid black",
-                  color: "white",
-                  fontWeight: "900",
-                  '& .MuiAlert-icon': {
-                    color: "white",
-                    fontWeight: "900",
-                  },
-                  '& .MuiAlert-action': {
-                    '& .MuiIconButton-root': {
-                      color: "white",
-                      fontWeight: "900",
-                    }
-                  }
-                }
-              }}
-        >
-            <Alert onClose={() => setAlertState(false)} severity={type}>
-                {message}
-            </Alert>
-        </Snackbar>
-    );
+	return (
+		<Snackbar open={state} autoHideDuration={5000} onClose={() => setAlertState(false)} anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+			sx={{
+				'& .MuiPaper-root': {
+					background: `${backgroundColor}`,
+					border: "6px solid black",
+					color: "white",
+					fontWeight: "900",
+					'& .MuiAlert-icon': {
+						color: "white",
+						fontWeight: "900",
+					},
+					'& .MuiAlert-action': {
+						'& .MuiIconButton-root': {
+							color: "white",
+							fontWeight: "900",
+						}
+					}
+				}
+			}}
+		>
+			<Alert onClose={() => setAlertState(false)} severity={type}>
+				{message}
+			</Alert>
+		</Snackbar>
+	);
 }

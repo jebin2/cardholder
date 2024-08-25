1c7a4b5e06b264ff0831d15417ba25e53699283f
jebin2 <jebineinstein@gmail.com>
2024-08-25 20:24:55 +0530

    Updates

--- precache-manifest.f9d01535f6eab906060ee20831ea82d5.js
+++ precache-manifest.f9d01535f6eab906060ee20831ea82d5.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7a4231c7779f3de89fa15afab71b01ab",
+    "revision": "f9aaecaab568eb8b39efc99a7160ebaa",
     "url": "/cardholder/index.html"
   },
   {
-    "revision": "539df3d4a66f57a85234",
+    "revision": "9a464f8ce9c74390edb8",
     "url": "/cardholder/static/css/main.69765cf6.chunk.css"
   },
   {
-    "revision": "1ce60fa74df435d8531e",
-    "url": "/cardholder/static/js/2.23ebe2fd.chunk.js"
+    "revision": "4558c3acf4ebad56a5f9",
+    "url": "/cardholder/static/js/2.5812b807.chunk.js"
   },
   {
     "revision": "85b57216247c06f1d678660f55e0f4fd",
-    "url": "/cardholder/static/js/2.23ebe2fd.chunk.js.LICENSE.txt"
+    "url": "/cardholder/static/js/2.5812b807.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "539df3d4a66f57a85234",
-    "url": "/cardholder/static/js/main.159dd8b9.chunk.js"
+    "revision": "9a464f8ce9c74390edb8",
+    "url": "/cardholder/static/js/main.d3372e15.chunk.js"
   },
   {
     "revision": "8e26e99c280b13a49c71",
